81d9e384694b40fc28ea0de8ec063e0d00ddeadf
ReignTech <reigntech@gmail.com>
2015-11-10 15:04:51 -0800

    Changed Author

--- FanManager_01.01/src/FanManager/FanManager.java
+++ FanManager_01.01/src/FanManager/FanManager.java
@@ -1,3 +1,5 @@
+
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -20,7 +22,7 @@ import javafx.stage.Stage;
 
 /**
  *
- * @author Felix
+ * @author Reign
  *      
  */
 public class FanManager extends Application {
