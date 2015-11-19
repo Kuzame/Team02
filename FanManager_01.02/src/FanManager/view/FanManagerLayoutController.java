d3a978108343caedb2f4a42a5877a53dcb9a65f7
ReignTech <reigntech@gmail.com>
2015-11-19 01:33:45 -0800

    Merged FXML Branch w/ Network Functionality

--- FanManager_01.02/src/FanManager/view/FanManagerLayoutController.java
+++ FanManager_01.02/src/FanManager/view/FanManagerLayoutController.java
@@ -5,57 +5,23 @@
  */
 package FanManager.view;
 
-<<<<<<< HEAD
-import FanManager.FanManager;
-=======
->>>>>>> Fixed FXML elements
+import FanManager.model.Fan;
+import FanManager.model.FanGroup;
 import FanManager.model.FanPane;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
 import java.net.URL;
+import java.util.Date;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-<<<<<<< HEAD
-import javafx.scene.Scene;
-=======
->>>>>>> Fixed FXML elements
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
 import javafx.scene.layout.TilePane;
-<<<<<<< HEAD
-import javafx.stage.Stage;
-/**
- *
- * @author Reign
- */
-
-
-public class FanManagerLayoutController implements Initializable  {
-    private static final double WIDTH = 1050, HEIGHT = 768;
-    FanPane[] fanPanes = new FanPane[6];
-    
-
-    @FXML
-    private TextField temperatureLabel;
-    @FXML
-    private TextField barometerLabel;
-    @FXML
-    private TextField humidityLabel;
-
-    // Reference to the main application.
-    private FanManager mainApp;
-
-    /**
-     * The constructor.
-     * The constructor is called before the initialize() method.
-     * @param primaryStage
-     */
-    public FanManagerLayoutController() {
-    }
-    public FanManagerLayoutController(Stage primaryStage) {
-            HBox box = new HBox();
-=======
 
 /**
  * FXML Controller class
@@ -73,39 +39,27 @@ public class FanManagerLayoutController implements Initializable {
     private TilePane fanPane;
     @FXML
     private ScrollPane scrollPane;
+    
+    
+    private int FANCOUNT = 6;
+    
+    
+    FanPane[] fanPanes = new FanPane[FANCOUNT];
+    private Fan[] fanArray = new Fan[FANCOUNT];
+    FanGroup fanGroup;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        FanPane[] fanPanes = new FanPane[6];
->>>>>>> Fixed FXML elements
+
         
         // Create fan panes; current 6
         for (int i = 0; i < fanPanes.length; i++) {
             fanPanes[i] = new FanPane();
         }
 
-<<<<<<< HEAD
-        primaryStage.setWidth(1013);
-        primaryStage.setHeight(768);
-        primaryStage.setResizable(false);
-        primaryStage.setScene(new Scene(box, WIDTH, HEIGHT));
-        primaryStage.setTitle("Fan Manager");
-
-        // Create a scroll pane for multiple fan blades
-        ScrollPane scrollPane = new ScrollPane();
-        scrollPane.setFitToHeight(true);
-        scrollPane.setFitToWidth(true);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
-
-        
-        // Add scroll panel to HBox container
-        box.getChildren().add(scrollPane);
-        HBox.setHgrow(scrollPane, Priority.ALWAYS);
-
-=======
->>>>>>> Fixed FXML elements
         // Create tile pane for fan panes
         TilePane tilePane = new TilePane();
         tilePane.setPrefColumns(3);
@@ -115,44 +69,114 @@ public class FanManagerLayoutController implements Initializable {
         for (FanPane fanPane : fanPanes) {
             tilePane.getChildren().add(fanPane);
         }
+        Thread networking = new Thread(new Networking());
+        networking.setDaemon(true);
+        networking.start(); /* network hanging in background after app is closed */
 
         // Add tile pane to scroll pane
         scrollPane.setContent(tilePane);
-<<<<<<< HEAD
-        scrollPane.getStylesheets().add(getClass().getResource("view/knobObject.css").toExternalForm());
-    }
-
-    public double getSampleWidth() {
-        return 495;
-    }
-
-    public double getSampleHeight() {
-        return 480;
-    }
-    
-
-    /**
-     * Initializes the controller class. This method is automatically called
-     * after the fxml file has been loaded.
-     */
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        // TODO
-    }    
-
-    /**
-     * Is called by the main application to give a reference back to itself.
-     * 
-     * @param mainApp
-     */
-    public void setMainApp(FanManager mainApp) {
-        this.mainApp = mainApp;
-
-    }
-}
-=======
         scrollPane.getStylesheets().add("FanManager/view/knobObject.css");
+
     }    
-  
+      /*
+     *   Private inner class to handle networking
+     */
+    class Networking implements Runnable {
+
+        private ServerSocket serverSocket;
+        private Socket socket;
+
+        private ObjectOutputStream toClient;
+        private ObjectInputStream fromClient;
+
+        @Override
+        public void run() {
+            try {
+        System.out.println("Networking");
+                // Create a server socket
+                serverSocket = new ServerSocket(8000);
+                System.out.println("Server started at " + new Date() + '\n');
+
+                // Listen for a connection request
+                socket = serverSocket.accept();
+
+                // Create data streams
+                toClient = new ObjectOutputStream(socket.getOutputStream());
+                toClient.flush();
+
+                // Create data input
+                fromClient = new ObjectInputStream(socket.getInputStream());
+
+                // Handle data
+                sendData();
+
+            } catch (IOException ex) {
+                        System.out.println("Networking try1");
+                System.err.println(ex);
+            } finally {
+                try {
+                    socket.close();
+                    serverSocket.close();
+                } catch (SocketException ex) {
+                        System.out.println("Networking finally catch1");
+
+                    System.err.println(ex);
+                } catch (IOException ex) {
+                        System.out.println("Networking finally catch2");
+
+                    System.err.println(ex);
+                }
+            }
+        }
+
+        private void sendData() {
+            try {
+                    System.out.println("sendData try");
+
+                while (true) {
+                    System.out.println("sendData while");
+                    // Read from network
+                    fanGroup = (FanGroup) fromClient.readObject();
+
+                    // Update gauge animations
+                    for (int i = 0; i < fanGroup.getFans().size(); i++) {
+                        fanPanes[i].setFan(fanGroup.getFans().get(i));
+//                        fanPanes[i].updateGauge();
+                    }
+
+                    // Write to network
+                    toClient.writeObject(fanGroup);
+                    toClient.reset();
+                    toClient.flush();
+
+                    Thread.sleep(1000);
+
+                }
+            } catch (ClassNotFoundException ex) {
+                System.out.println("sendData catch 1");
+                System.err.println(ex);
+            } catch (InterruptedException ex) {
+                System.out.println("sendData catch 2");
+                ex.printStackTrace();
+            } catch (IOException ex) {
+                System.out.println("sendData catch 3");
+                System.err.println(ex);
+            } finally {
+                try {
+                    System.out.println("sendData finally try 1");
+                    socket.close();
+                    fromClient.close();
+                    toClient.close();
+                } catch (SocketException ex) {
+                    System.out.println("sendData catch 3");
+                    System.err.println(ex);
+                    System.out.println("Fan Server Error: Finally Block");
+                } catch (IOException ex) {
+                    System.out.println("sendData catch 3");
+                    System.err.println(ex);
+                }
+
+            }
+        }
+    }
 }
->>>>>>> Fixed FXML elements
