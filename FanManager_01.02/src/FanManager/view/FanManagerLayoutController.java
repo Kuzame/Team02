2b61253ba56436956ed6694191c371d5cd387d7b
Felix Alonso <Xleif5@gmail.com>
2015-11-25 14:08:03 -0800

    Remove Networking inner class from FanManagerLayoutController; Create separate class file for Networking class; Add getter methods getFanList, getFanGroup, getFanPanes for FanManagerLayoutController;

--- FanManager_01.02/src/FanManager/view/FanManagerLayoutController.java
+++ FanManager_01.02/src/FanManager/view/FanManagerLayoutController.java
@@ -8,15 +8,12 @@ package FanManager.view;
 import FanManager.model.Fan;
 import FanManager.model.FanGroup;
 import FanManager.model.FanPane;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.net.SocketException;
+import FanManager.Networking;
 import java.net.URL;
-import java.util.Date;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ScrollPane;
@@ -29,6 +26,7 @@ import javafx.scene.layout.TilePane;
  * @author reign
  */
 public class FanManagerLayoutController implements Initializable {
+
     @FXML
     private TextField temperatureLabel;
     @FXML
@@ -39,13 +37,12 @@ public class FanManagerLayoutController implements Initializable {
     private TilePane fanPane;
     @FXML
     private ScrollPane scrollPane;
-    
-    
+
     private int FANCOUNT = 6;
-    
-    
+
     FanPane[] fanPanes = new FanPane[FANCOUNT];
-    private Fan[] fanArray = new Fan[FANCOUNT];
+    private ObservableList<Fan> fanList = FXCollections.observableArrayList();
+//    private Fan[] fanArray = new Fan[FANCOUNT];
     FanGroup fanGroup;
 
     /**
@@ -54,10 +51,10 @@ public class FanManagerLayoutController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
 
-        
         // Create fan panes; current 6
         for (int i = 0; i < fanPanes.length; i++) {
-            fanPanes[i] = new FanPane();
+            fanPanes[i] = new FanPane(this);
+            fanList.add(fanPanes[i].getFan());
         }
 
         // Create tile pane for fan panes
@@ -69,114 +66,37 @@ public class FanManagerLayoutController implements Initializable {
         for (FanPane fanPane : fanPanes) {
             tilePane.getChildren().add(fanPane);
         }
-        Thread networking = new Thread(new Networking());
+
+        fanGroup = new FanGroup(fanList.toArray(new Fan[0]),
+                (fanList.get(0)).getTemperature(), 40, 50);
+
+        Thread networking = new Thread(new Networking(this));
         networking.setDaemon(true);
-        networking.start(); /* network hanging in background after app is closed */
+        networking.start();
 
         // Add tile pane to scroll pane
         scrollPane.setContent(tilePane);
         scrollPane.getStylesheets().add("FanManager/view/knobObject.css");
 
-    }    
-      /*
-     *   Private inner class to handle networking
-     */
-    class Networking implements Runnable {
-
-        private ServerSocket serverSocket;
-        private Socket socket;
-
-        private ObjectOutputStream toClient;
-        private ObjectInputStream fromClient;
-
-        @Override
-        public void run() {
-            try {
-        System.out.println("Networking");
-                // Create a server socket
-                serverSocket = new ServerSocket(8000);
-                System.out.println("Server started at " + new Date() + '\n');
-
-                // Listen for a connection request
-                socket = serverSocket.accept();
-
-                // Create data streams
-                toClient = new ObjectOutputStream(socket.getOutputStream());
-                toClient.flush();
-
-                // Create data input
-                fromClient = new ObjectInputStream(socket.getInputStream());
-
-                // Handle data
-                sendData();
-
-            } catch (IOException ex) {
-                        System.out.println("Networking try1");
-                System.err.println(ex);
-            } finally {
-                try {
-                    socket.close();
-                    serverSocket.close();
-                } catch (SocketException ex) {
-                        System.out.println("Networking finally catch1");
-
-                    System.err.println(ex);
-                } catch (IOException ex) {
-                        System.out.println("Networking finally catch2");
-
-                    System.err.println(ex);
-                }
-            }
-        }
-
-        private void sendData() {
-            try {
-                    System.out.println("sendData try");
-
-                while (true) {
-                    System.out.println("sendData while");
-                    // Read from network
-                    fanGroup = (FanGroup) fromClient.readObject();
-
-                    // Update gauge animations
-                    for (int i = 0; i < fanGroup.getFans().size(); i++) {
-                        fanPanes[i].setFan(fanGroup.getFans().get(i));
-                        fanPanes[i].updateGauge();
-                    }
-
-                    // Write to network
-                    toClient.writeObject(fanGroup);
-                    toClient.reset();
-                    toClient.flush();
-
-                    Thread.sleep(1000);
-
-                }
-            } catch (ClassNotFoundException ex) {
-                System.out.println("sendData catch 1");
-                System.err.println(ex);
-            } catch (InterruptedException ex) {
-                System.out.println("sendData catch 2");
-                ex.printStackTrace();
-            } catch (IOException ex) {
-                System.out.println("sendData catch 3");
-                System.err.println(ex);
-            } finally {
-                try {
-                    System.out.println("sendData finally try 1");
-                    socket.close();
-                    fromClient.close();
-                    toClient.close();
-                } catch (SocketException ex) {
-                    System.out.println("sendData catch 3");
-                    System.err.println(ex);
-                    System.out.println("Fan Server Error: Finally Block");
-                } catch (IOException ex) {
-                    System.out.println("sendData catch 3");
-                    System.err.println(ex);
-                }
-
-            }
-        }
     }
-}
+
+    public ObservableList<Fan> getFanList() {
+        return fanList;
+    }
+
+    public FanGroup getFanGroup() {
+        fanGroup.setFans(new ArrayList<Fan>(fanList));
+        return fanGroup;
+    }
+
+    public FanPane[] getFanPanes() {
+        return fanPanes;
+    }
+
+    public void updateFanList(double speed, int id) {
+        //fanList.get(id).setSpeed(speed);
+        fanList.set(id, new Fan(speed, fanList.get(id)));
+        fanPanes[id].setFan(fanList.get(id));
+    }
+
+   }
