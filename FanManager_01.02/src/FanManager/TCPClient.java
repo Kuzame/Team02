032981aeae49633524ad35aa4a51bd8b586a77cb
ReignTech <reigntech@gmail.com>
2015-12-05 16:52:13 -0800

    receiving int from prototype. Console (work in progress) sending int to prototype. Communicating with Prototype!!!

--- FanManager_01.02/src/FanManager/TCPClient.java
+++ FanManager_01.02/src/FanManager/TCPClient.java
@@ -1,4 +1,4 @@
-/*
+/*Working TCPClient.java error 1st char
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -13,26 +13,36 @@ import FanManager.model.Fan;
 import FanManager.model.FanGroup;
 import FanManager.model.FanPane;
 import FanManager.view.FanManagerLayoutController;
-import java.io.*;  
-import java.net.*; 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ConnectException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
 import java.util.Date;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 
+public class TCPClient implements Runnable {
 
-public class TCPClient implements Runnable {  
+    private DataOutputStream toPrototype;
+    private DataInputStream fromPrototype;
+//    private ObjectOutputStream toClient;
+//    private ObjectInputStream fromClient;
 
-    private ObjectOutputStream toClient;
-    private DataInputStream fromClient;
     private ServerSocket server;
     private Socket client;
+
+    private String consoleData; // logs data from prototype
 
     private FanManagerLayoutController mainApp;
     private FanGroup fanGroup;
     private ObservableList<Fan> fanList;
-    private FanPane [] fanPanes;
+    private FanPane[] fanPanes;
 
-    
     public TCPClient(FanManagerLayoutController mainApp) {
         this.mainApp = mainApp;
         fanList = this.mainApp.getFanList();
@@ -50,15 +60,17 @@ public class TCPClient implements Runnable {
             System.out.println("client = Server accepted");
 
             // Create an output stream to send data to the server
-            toClient = new ObjectOutputStream(client.getOutputStream());
-            System.out.println("toClient: " + toClient + '\n');
-            toClient.flush();
-            System.out.println("toClient flush: " + toClient + '\n');
- 
+//            toClient = new ObjectOutputStream(client.getOutputStream());
+//            System.out.println("toClient: " + toClient + '\n');
+//            toClient.flush();
+//            System.out.println("toClient flush: " + toClient + '\n');
             // Create an input stream to receive data from the server
-            fromClient = new DataInputStream(client.getInputStream());
-            System.out.println("fromClient: " + fromClient + '\n');
+            fromPrototype = new DataInputStream(client.getInputStream());
+            System.out.println("fromPrototype: " + fromPrototype + '\n');
 //                System.out.println(fromClient.readObject());
+            
+            toPrototype = new DataOutputStream(client.getOutputStream());
+            
 
             fanList.addListener(new ListChangeListener() {
                 @Override
@@ -67,12 +79,12 @@ public class TCPClient implements Runnable {
                     sendData();
                 }
             });
-            
+
             // Receive server data
             Thread recieveData = new Thread(() -> recieveData());
             recieveData.setDaemon(true);
             recieveData.start();
-            
+
 //            String sentence;   String modifiedSentence;   
 //            System.out.println("Inside aThis TCPClient RUN!");
 //
@@ -88,51 +100,54 @@ public class TCPClient implements Runnable {
 //            System.out.println("FROM SERVER: " + sentence); 
 //
 //            clientSocket.close();  
+        } catch (ConnectException ce) {
+            System.err.println(ce);
+        } catch (Exception ex) {
+            System.out.println("Bad TCPClient Connection");
+            System.err.println(ex);
 
-           } catch (ConnectException ce) {
-               System.err.println(ce);
-           } catch (Exception ex) {
-               System.out.println("Bad TCPClient Connection");
-                              System.err.println(ex);
-
-           }
         }
-    
-        private void recieveData() {
+    }
+
+    private void recieveData() {
         try {
             while (true) {
-                System.out.println("Waiting for object");
+//                System.out.println("Waiting for object");
                 // Read from network
-                int theObject = fromClient.read();
-//                FanGroup theObject = (FanGroup) fromClient.readObject();
-//    System.setErr(new PrintStream(Console.getInstance(theObject)));
-                
-                
-                System.out.println("Recieved char");
-                System.out.println(theObject);
+//                char someInt = fromPrototype.readChar();
+                char someInt = (char) fromPrototype.read();
+                consoleData += someInt;
+                if (someInt == '\r') {
+                    //data += "\n";
+                    System.out.println(consoleData);
 
-                // If new fanGroup sent over, then update each fan
-                if (!fanGroup.equals(theObject)) {
-//                    fanGroup.update(theObject);
-                    
-                    // Update fan animations
-                    for (int i = 0; i < fanGroup.getFans().size(); i++) {
-                        if (!fanList.get(i).equals(fanGroup.getFans().get(i))) {
-                            fanPanes[i].setFan(fanGroup.getFans().get(i));
-                            fanPanes[i].updateGauge();
-                        }
-                    }
                 }
+//                FanGroup temp = (FanGroup) fromClient.readObject();
+//                System.setErr(new PrintStream(Console.getInstance(theObject)));
 
+                //System.out.println("Recieved Char");
+                //System.out.println(someInt);
+//                // If new fanGroup sent over, then update each fan
+//                if (!fanGroup.equals(someInt)) {
+//                    fanGroup.update(someInt);
+//                    
+//                    // Update fan animations
+//                    for (int i = 0; i < fanGroup.getFans().size(); i++) {
+//                        if (!fanList.get(i).equals(fanGroup.getFans().get(i))) {
+//                            fanPanes[i].setFan(fanGroup.getFans().get(i));
+//                            fanPanes[i].updateGauge();
+//                        }
+//                    }
+//                }
                 Thread.sleep(10);
             }
         } catch (Exception ex) {
             ex.printStackTrace();
         } finally {
             try {
-                client.close();
-                fromClient.close();
-                toClient.close();
+//                client.close();
+                fromPrototype.close();
+//                toClient.close();
             } catch (SocketException ex) {
                 System.err.println(ex);
                 System.out.println("Fan Server Error: Finally Block");
@@ -145,20 +160,24 @@ public class TCPClient implements Runnable {
 
     private void sendData() {
         try {
-                toClient.reset();
-                System.out.println("sending data");
-                fanGroup = mainApp.getFanGroup();
-                //System.out.println(fanGroup.getFans().get(0).getSpeed());
+            
+            
+            System.out.println("sending data");
+            fanGroup = mainApp.getFanGroup();
+            int intSpeed = (int)fanGroup.getFans().get(0).getSpeed();
+            char charSpeed = (char)intSpeed;
+            
+            System.out.println(fanGroup.getFans().get(0).getSpeed());
 
-                // Write to network
-                toClient.writeObject(fanGroup);
-                toClient.flush();
-                
-                System.out.println("data sent");
+            // Write to network
+            toPrototype.write(intSpeed);
+            toPrototype.flush();
 
-                
+            
+            System.out.println("data sent");
+
         } catch (Exception ex) {
             ex.printStackTrace();
-        } 
+        }
     }
-    } 
+}
